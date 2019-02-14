30fccde5bd083da8643eee5de559935f73efa200
amoussav <38815455+amoussav@users.noreply.github.com>
2019-02-13 23:50:04 -0500

    Add files via upload

--- DA_2/js/main.js
+++ DA_2/js/main.js
@@ -11,41 +11,88 @@ window.onload = function() {
     // All loading functions will typically all be found inside "preload()".
 
 
-    var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', {
+    var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', {
         preload: preload,
         create: create,
         update: update,
-        render: render
     });
 
     function preload() {
-        game.load.image('arrow', 'assets/sprites/arrow.png');
+        game.load.image('guy', 'assets/sprites/guy.png');
+        game.load.spritesheet('dude', 'assets/sprites/dude.png', 32, 48);
+        game.load.image('bg', 'assets/sprites/bg.png');
+        game.load.image('ground', 'assets/sprites/platform.png');
     }
 
-    var sprite;
+    var platforms;
+    var player;
+    var cursors;
 
     function create() {
-
+        //physics system
         game.physics.startSystem(Phaser.Physics.ARCADE);
 
-        game.stage.backgroundColor = '#0072bc';
+        //background
+        game.add.sprite(0, 0, 'bg');
 
-        sprite = game.add.sprite(400, 300, 'arrow');
-        sprite.anchor.setTo(0.5, 0.5);
+        //ground
+       platforms = game.add.group();
+       platforms.enableBody = true;
+       var ground = platforms.create(0, game.world.height - 64, 'ground');
+       ground.scale.setTo(2, 2);
+       ground.body.immovable = true;
 
+       //platforms
+       var ledge = platforms.create(200, 400, 'ground');
+       ledge.body.immovable = true;
+
+       //player
+        player = game.add.sprite(32, game.world.height - 150, 'dude');
+        game.physics.arcade.enable(player);
+        player.body.bounce.y = 0.2;
+        player.body.gravity.y = 1200;
+        player.body.collideWorldBounds = true;
+        player.animations.add('left', [0, 1, 2, 3], 10, true);
+        player.animations.add('right', [5, 6, 7, 8], 10, true);
+
+        //cursor
+        cursors = game.input.keyboard.createCursorKeys();
     }
 
     function update() {
 
-        //  This will update the sprite.rotation so that it points to the currently active pointer
-        //  On a Desktop that is the mouse, on mobile the most recent finger press.
-        sprite.rotation = game.physics.arcade.angleToPointer(sprite);
+        //collide with platforms
+        var hitPlatforms = game.physics.arcade.collide(player, platforms);
 
-    }
+        //  Reset the players velocity (movement)
+        player.body.velocity.x = 0;
 
-    function render() {
+        if (cursors.left.isDown)
+        {
+            //  Move to the left
+            player.body.velocity.x = -300;
 
-        game.debug.spriteInfo(sprite, 32, 32);
+            player.animations.play('left');
+        }
+        else if (cursors.right.isDown)
+        {
+            //  Move to the right
+            player.body.velocity.x = 300;
 
+            player.animations.play('right');
+        }
+        else
+        {
+            //  Stand still
+            player.animations.stop();
+
+            player.frame = 4;
+        }
+
+        //  Allow the player to jump if they are touching the ground.
+        if (cursors.up.isDown && player.body.touching.down)
+        {
+            player.body.velocity.y = -640;
+        }
     }
 }
