96df2681c66ee7361a45b856702bb47a1ac3882d
amoussav <38815455+amoussav@users.noreply.github.com>
2019-02-21 01:28:25 -0500

    Add files via upload

---
+++ DA_3/js/main.js
@@ -0,0 +1,92 @@
+"use strict";
+
+window.onload = function() {
+    // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
+    // You will need to change the fourth parameter to "new Phaser.Game()" from
+    // 'phaser-example' to 'game', which is the id of the HTML element where we
+    // want the game to go.
+    // The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
+    // You will need to change the paths you pass to "game.load.image()" or any other
+    // loading functions to reflect where you are putting the assets.
+    // All loading functions will typically all be found inside "preload()".
+
+
+    var game = new Phaser.Game(800, 704, Phaser.AUTO, 'game', {
+        preload: preload,
+        create: create,
+        update: update,
+    });
+
+    function preload() {
+        game.load.tilemap('map', 'assets/map.csv', null, Phaser.Tilemap.CSV);
+        game.load.image('tiles', 'assets/tiles.png');
+        game.load.spritesheet('player', 'assets/sprite2.png');
+        game.load.image('book', 'assets/book.png');
+        game.load.image('bookcheck', 'assets/bookcheck.png');
+    }
+
+    var map;
+    var layer;
+    var player;
+    var cursors;
+    var books;
+    var book1, book2;
+
+    function create() {
+        //map
+        map = game.add.tilemap('map', 32, 32);
+        map.addTilesetImage('tiles');
+        layer = map.createLayer(0);
+        layer.resizeWorld();
+        map.setCollision(1);
+        //layer.debug = true;
+
+        //books
+        books = game.add.group();
+        books.enableBody = true;
+        book1 = books.create(96, 96, 'book');
+        book2 = books.create(200, 200, 'book');
+
+        //player
+        player = game.add.sprite(32, 32, 'player', 0);
+        game.physics.enable(player, Phaser.Physics.ARCADE);
+        game.camera.follow(player);
+        cursors = game.input.keyboard.createCursorKeys();
+    }
+
+    function update() {
+        game.physics.arcade.collide(player, layer);
+
+        //overlap with yearbooks
+        game.physics.arcade.overlap(player, books, replaceBook);
+
+        //movement
+        player.body.velocity.set(0);
+
+        if (cursors.left.isDown && !cursors.right.isDown)
+        {
+            player.body.velocity.x = -100;
+            player.play('left');
+        }
+        if (cursors.right.isDown && !cursors.left.isDown)
+        {
+            player.body.velocity.x = 100;
+            player.play('right');
+        }
+        if (cursors.up.isDown && !cursors.down.isDown)
+        {
+            player.body.velocity.y = -100;
+            player.play('up');
+        }
+        if (cursors.down.isDown && !cursors.up.isDown)
+        {
+            player.body.velocity.y = 100;
+            player.play('down');
+        }
+            player.animations.stop();
+    }
+
+    function replaceBook(player, book) {
+        book.loadTexture('bookcheck');
+    }
+}
